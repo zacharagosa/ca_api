d3a60fcc47a117f2f2a77926867075dfc4f8a658
zacharagosa <zachary.aragosa@looker.com>
2025-12-11 13:59:40 -0500

    Fix deployment script: Add project flag and missing env vars

--- deploy_cloud_run.sh
+++ deploy_cloud_run.sh
@@ -13,9 +13,10 @@ cd ..
 
 echo "Deploying to Cloud Run..."
 gcloud run deploy ca-api \
+  --project ${PROJECT_ID} \
   --source . \
   --region us-central1 \
   --allow-unauthenticated \
-  --set-env-vars PROJECT_ID=${PROJECT_ID},LOCATION=${LOCATION},LOOKER_CLIENT_ID=${LOOKER_CLIENT_ID},LOOKER_CLIENT_SECRET=${LOOKER_CLIENT_SECRET},LOOKER_INSTANCE_URI=${LOOKER_INSTANCE_URI}
+  --set-env-vars PROJECT_ID=${PROJECT_ID},LOCATION=${LOCATION},LOOKER_CLIENT_ID=${LOOKER_CLIENT_ID},LOOKER_CLIENT_SECRET=${LOOKER_CLIENT_SECRET},LOOKER_INSTANCE_URI=${LOOKER_INSTANCE_URI},LOOKML_MODEL=${LOOKML_MODEL},EXPLORE=${EXPLORE}
 
 echo "Deployment complete!"
